c0c745e44b4856cd16938a1ca18bc43dbc06c374
graciliana <eagraciliana@gmail.com>
2026-05-27 19:59:13 -0300

    [feat] criado as funcoes tratamento de dados e

---
+++ src/functions.py
@@ -0,0 +1,103 @@
+# limpeza
+# regex
+# # datas 
+# médias
+# vaidaçãooes
+import re
+from datetime import datetime
+
+# remover espaço colocar tudo com letras minusculas e remover caraters
+def limpar_categoria(categoria):
+
+    categoria = categoria.lower()
+
+    categoria = categoria.strip()
+
+    categoria = re.sub(
+        r"[^a-zA-Zà-ÿ0-9\s]",
+        "",
+        categoria
+    )
+
+    categoria = re.sub(r"\s+", " ", categoria)
+
+    return categoria
+# tratar as categoria vazias substituindo  por sem categoria
+def tratar_categoria_vazia(categoria):
+    
+    if categoria.strip() == "":
+        
+        return "sem categoria"
+    
+    return categoria
+
+# converter datas
+def converter_data(data):
+    
+    if data.strip() == "":
+        return ""
+    data_convertida = datetime.strptime(
+        data,
+        "%Y-%m-%d %H:%M:%S"
+    )
+    
+    return data_convertida.strftime("%d/%m/%Y")
+
+## cALCULAR MÉDIA
+def calcular_media(lista_valores):
+    soma = sum(lista_valores)
+    quantidade = len(lista_valores)
+    media = soma / quantidade
+    
+    return round(media, 2)
+
+# tratar dimensoes vazias 
+def tratar_dimensao(valor, media):
+
+    if valor.strip() == "":
+
+        return media
+
+    return float(valor)
+
+# calcular volume do produto
+
+def calcular_volume(
+    comprimento,
+    altura,
+    largura
+):
+    if (
+        comprimento == ""
+        or altura == ""
+        or largura == ""
+    ):
+        return 0
+
+    volume = (
+        float(comprimento)
+        * float(altura)
+        * float(largura)
+    )
+
+    return round(volume, 2)
+
+if __name__ == "__main__":
+
+    print(limpar_categoria(" Informática### "))
+    print(limpar_categoria("Cama, Mesa e Banho!!!"))
+    print(limpar_categoria("Telefonia@#$"))
+    
+    print(tratar_categoria_vazia(""))
+    
+
+    data = "2017-05-16 15:05:35"
+
+    print(converter_data(data))
+    
+    valores = [10,20,30]
+    print(calcular_media(valores))
+    
+    print(tratar_dimensao("", 250.0))
+    
+    print(calcular_volume(20, 10, 0))
